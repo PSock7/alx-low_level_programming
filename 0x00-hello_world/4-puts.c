1ab0dbab5aeb691aeb6e6d56af5313a3db9af3d5
mouhamed sock <papesock1@gmail.com>
2022-09-08 02:57:53 -0700

    fixed betty style 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
-*main - Main function of my project
-*
-*
-*Return: return 0 everytime
-*/
+ * main - Enter function and use puts
+ 
+ *
+ * Return: Always 0 Success
+ */
+
+
 int main(void){
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
